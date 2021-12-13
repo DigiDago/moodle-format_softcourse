6f71deecc64952c3724cab41fac3061ceece7f44
CrymSonir <revenu.sylvain@gmail.com>
2021-12-13 10:56:21 +0100

    Review render of section with render_content method

--- classes/output/courseformat/content/section.php
+++ classes/output/courseformat/content/section.php
@@ -43,6 +43,7 @@ class section extends section_base {
     protected $format;
 
     public function export_for_template(\renderer_base $output): stdClass {
+        // Review this part.
         $format = $this->format;
 
         $data = parent::export_for_template($output);
@@ -50,123 +51,116 @@ class section extends section_base {
         $course = $format->get_course();
         $modinfo = get_fast_modinfo($course);
 
-        $sectionmods = [];
-
         $completioninfo = new completion_info($course);
         $context = context_course::instance($course->id);
 
         // If section have no mods we skip.
         if (!$data->cmlist->cms) {
             $data->skip = true;
+            return $data;
         }
 
-        // Assiociation tabs : section[id_of_section] = cm.
-        foreach ($data->cmlist->cms as $cm) {
+        // Prepare some cm_info we will need further.
+        foreach ($data->cmlist->cms as $key => $cm) {
             $cm = $modinfo->get_cm($cm->cmitem->id);
             $idsection = $cm->get_section_info()->section;
-
             // Hide the section 0 if the course format option is set to "Hide the section 0".
             if (!($idsection == 0 && $format->get_format_options()['hidesectionzero'] == 1)) {
                 $info = $modinfo->get_section_info_all()[$idsection];
-                if (!isset($sectionmods[$idsection])) {
-                    $sectionmods[$idsection] = new stdClass();
-                    $sectionmods[$idsection]->cm = [];
-                    $sectionmods[$idsection]->id = $idsection;
-                    $sectionmods[$idsection]->name = $info->name;
+                if ($data) {
+                    $data->idsection = $idsection;
+                    $data->name = $info->name;
                     $summary = new stdClass();
                     $summary->summarytext = $info->summary;
-                    $sectionmods[$idsection]->summary = $summary;
-                    $sectionmods[$idsection]->uservisible = $info->uservisible;
-                    $sectionmods[$idsection]->visible = $info->visible;
-                    $sectionmods[$idsection]->available = $info->available;
+                    $data->summary = $summary;
+                    $data->uservisible = $info->uservisible;
+                    $data->visible = $info->visible;
+                    $data->available = $info->available;
                 }
-                $sectionmods[$idsection]->cm[] = $cm;
+                $data->cmlist->cms[$key]->cminfo = $cm;
+            } else {
+                $data->skip = true;
+                return $data;
             }
         }
-        // Put tabs into a tabs readable by mustache.
 
-        foreach ($sectionmods as $section) {
-            // We check case were section are hidden.
-            // We check case were section have only one hidden activity.
-            if ($section->visible == 0 || $section->uservisible == false || $section->available == false) {
-                $data->skip = true;
-            } else if (count($section->cm) == 1
-                && ($section->cm[0]->visible == 0
-                    || $section->cm[0]->visibleoncoursepage == 0
-                    || $section->cm[0]->uservisible == false || $section->cm[0]->available == false)
-            ) {
-                $data->skip = true;
-            }
-            $s = $data;
-            $s->name = format_string(
-                $section->name,
-                true,
-                array('context' => context_course::instance($course->id))
+        // We check case were section are hidden.
+        // We check case were section have only one hidden activity.
+        if ($data->visible == 0 || $data->uservisible == false || $data->available == false) {
+            $data->skip = true;
+            return $data;
+        } else if (count($data->cmlist->cms) == 1
+            && ($data->cmlist->cms[0]->cminfo->visible == 0
+                || $data->cmlist->cms[0]->cminfo->visibleoncoursepage == 0
+                || $data->cmlist->cms[0]->cminfo->uservisible == false || $data->cmlist->cms[0]->cminfo->available == false)
+        ) {
+            $data->skip = true;
+            return $data;
+        }
+        $data->name = format_string(
+            $data->name,
+            true,
+            array('context' => context_course::instance($course->id))
+        );
+        $data->courseid = $course->id;
+        $options = new stdClass();
+        $options->noclean = true;
+        $options->overflowdiv = true;
+        $data->summary->summarytext = format_text($data->summary->summarytext, 1, $options);
+        $data->start = get_string('startcourse', 'format_softcourse');
+        $data->countactivitiestooltip = get_string('countactivities', 'format_softcourse');
+        $data->countactivities = 0;
+
+        // Check capability to edit/delete softcourse section picture.
+        if (has_capability('moodle/course:update', $context)) {
+            $data->update_img = get_string('update_img', 'format_softcourse');
+            $data->delete_img = get_string('delete_img', 'format_softcourse');
+        }
+
+        // Render the iamge section.
+        $fs = get_file_storage();
+        $file = $fs->get_area_files($context->id, 'format_softcourse', 'sectionimage', $data->id,
+            "itemid, filepath, filename", false);
+        if ($file) {
+            $data->urlimg = \moodle_url::make_pluginfile_url(
+                end($file)->get_contextid(),
+                end($file)->get_component(),
+                end($file)->get_filearea(),
+                end($file)->get_itemid(),
+                end($file)->get_filepath(),
+                end($file)->get_filename()
             );
-            $s->courseid = $course->id;
-            $options = new stdClass();
-            $options->noclean = true;
-            $options->overflowdiv = true;
-            $s->summary->summarytext = format_text($section->summary->summarytext, 1, $options);
-            $s->start = get_string('startcourse', 'format_softcourse');
-            $s->countactivitiestooltip = get_string('countactivities', 'format_softcourse');
-            $s->countactivities = 0;
+        }
 
-            if (has_capability('moodle/course:update', $context)) {
-                $s->update_img = get_string('update_img', 'format_softcourse');
-                $s->delete_img = get_string('delete_img', 'format_softcourse');
-            }
-
-            // Render the iamge section.
-            $fs = get_file_storage();
-            $file = $fs->get_area_files($context->id, 'format_softcourse', 'sectionimage', $s->id,
-                "itemid, filepath, filename", false);
-            if ($file) {
-                $s->urlimg = \moodle_url::make_pluginfile_url(
-                    end($file)->get_contextid(),
-                    end($file)->get_component(),
-                    end($file)->get_filearea(),
-                    end($file)->get_itemid(),
-                    end($file)->get_filepath(),
-                    end($file)->get_filename()
-                );
-            }
-
-            $nbcomplete = 0;
-            $nbcompletion = 0;
-
-            $s->first_cm_url = '';
-            $s->countactivities = 0;
-            // Get completion of cms.
-            foreach ($section->cm as $cm) {
-                if ($cm->available && ($cm->uservisible && !$cm->is_stealth() && $cm->modname != 'label'
-                        || !empty($cm->url)) && $s->first_cm_url == '') {
-                    if ($cm->modname == 'resource') {
-                        $cm->url->param('forceview', 1);
-                    }
-                    $s->first_cm_url = $cm->url->out(false);
+        $nbcomplete = 0;
+        $nbcompletion = 0;
+        $data->first_cm_url = '';
+        $data->countactivities = 0;
+        // Get completion of cms.
+        foreach ($data->cmlist->cms as $cm) {
+            if ($cm->cminfo->available && ($cm->cminfo->uservisible && !$cm->cminfo->is_stealth() && $cm->cminfo->modname != 'label'
+                    || !empty($cm->url)) && $data->first_cm_url == '') {
+                if ($cm->cminfo->modname == 'resource') {
+                    $cm->cminfo->url->param('forceview', 1);
                 }
-                if ($cm->completion > 0) {
-                    $nbcompletion++;
-                }
-                $nbcomplete += $completioninfo->get_data($cm, true)->completionstate;
-                if ($cm->deletioninprogress == 0 && $cm->visible == 1
-                    && $cm->modname != "label" && $cm->visibleoncoursepage == 1
-                    && $cm->uservisible == true
-                    && $cm->available == true) {
-                    $s->countactivities += 1;
-                }
+                $data->first_cm_url = $cm->cminfo->url->out(false);
             }
-            // Count the percent of cm complete.
-            if ($nbcompletion != 0) {
-                $s->progression = get_string('progression', 'format_softcourse');
-                $percentcomplete = $nbcomplete * 100 / $nbcompletion;
-                $s->progression_percent = intval($percentcomplete);
+            if ($cm->cminfo->completion > 0) {
+                $nbcompletion++;
             }
-            $s->hasmodules = ($s->countactivities > 0);
-            if ($s->hasmodules) {
-                $data = $s;
+            $nbcomplete += $completioninfo->get_data($cm->cminfo, true)->completionstate;
+            if ($cm->cminfo->deletioninprogress == 0 && $cm->cminfo->visible == 1
+                && $cm->cminfo->modname != "label" && $cm->cminfo->visibleoncoursepage == 1
+                && $cm->cminfo->uservisible == true
+                && $cm->cminfo->available == true) {
+                $data->countactivities += 1;
             }
+        }
+        // Count the percent of cm complete.
+        if ($nbcompletion != 0) {
+            $data->progression = get_string('progression', 'format_softcourse');
+            $percentcomplete = $nbcomplete * 100 / $nbcompletion;
+            $data->progression_percent = intval($percentcomplete);
         }
 
         return $data;
