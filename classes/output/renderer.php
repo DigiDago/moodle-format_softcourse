4c3f642d7eea96120c22d20c9b89342cef1ee2c1
CrymSonir <revenu.sylvain@gmail.com>
2022-12-05 11:48:12 +0100

    Fix some issue with multiple case

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -111,11 +111,23 @@ class renderer extends section_renderer {
             $options->overflowdiv = true;
             $introduction = $this->courseformat->get_format_options()['introduction'];
             $data->courseintroduction = format_text($introduction, 1, $options);
+            $data->start_url = $data->initialsection->start_url;
 
             if ($this->courseformat->get_format_options()['hideallsections'] == 1) {
-                $data->sections = false;
+                if ($data->initialsection->skip != true) {
+                    $data->sections = false;
+                } else {
+                    if (!$data->start_url) {
+                        foreach ($data->sections as $section) {
+                            if ($section->skip != true && $section->start_url != null) {
+                                $data->start_url = $section->start_url;
+                                $data->sections = $section;
+                                break;
+                            }
+                        }
+                    }
+                }
             } else {
-                $data->start_url = $data->initialsection->start_url;
                 if (!$data->start_url) {
                     foreach ($data->sections as $section) {
                         if ($section->skip != true && $section->start_url != null) {
